291bdfe72e6e4b9c42b836cf970ebe5f6e038d22
na1110 <nagaitomoya1110@gmail.com>
2023-11-08 01:16:38 +0900

    Lexer.javaを初期化

---
+++ src/interpreter/com/na1110/Lexer.java
@@ -0,0 +1,33 @@
+package com.na1110;
+
+import java.io.Reader;
+
+public class Lexer {
+    private int tok;
+    private Object val;
+    private LexerReader reader;
+
+    public Lexer(Reader r) {
+        reader = new LexerReader(r);
+    }
+
+    public int token() {
+        return tok;
+    }
+
+    public Object value() {
+        return val;
+    }
+
+    public boolean advance() {
+        return false;
+    }
+
+    private void lexDigit() throws Exception {
+        
+    }
+
+    private void skipWhiteSpace() throws Exception {
+        
+    }
+}
