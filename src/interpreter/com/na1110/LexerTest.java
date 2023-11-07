a93665551efcf0c162858ccabf393eb85c39a113
na1110 <nagaitomoya1110@gmail.com>
2023-11-08 02:40:55 +0900

    LexerTest.javaを初期化

---
+++ src/interpreter/com/na1110/LexerTest.java
@@ -0,0 +1,43 @@
+package com.na1110;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+public class LexerTest {
+    public void testToken() {
+        Reader reader = new StringReader("1234");
+        Lexer lex = new Lexer(reader);
+        if (lex.advance()) {
+            if (lex.token() != TokenType.INT) {
+                System.out.println("fault");
+            }
+        } else {
+            System.out.println("falut");
+        }
+    }
+
+    public void testValue() {
+        Reader reader = new StringReader("1234  5678");
+        Lexer lex = new Lexer(reader);
+        if (lex.advance()) {
+            if (!lex.value().toString().equals("1234")) {
+                System.out.println("fault");
+            }
+        } else {
+            System.out.println("fault");
+        }
+        if (lex.advance()) {
+            if (!lex.value().toString().equals("5678")) {
+                System.out.println("fault");
+            }
+        } else {
+            System.out.println("falut");
+        }
+    }
+
+    public static void main(String[] args) {
+        LexerTest test = new LexerTest();
+        test.testToken();
+        test.testValue();
+    }
+}
