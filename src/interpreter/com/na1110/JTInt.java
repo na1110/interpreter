45491a786bb54902d23da893cc7940c1b4df3ee2
na1110 <nagaitomoya1110@gmail.com>
2023-11-09 12:03:50 +0900

    JTInt.javaを初期化

---
+++ src/interpreter/com/na1110/JTInt.java
@@ -0,0 +1,17 @@
+package com.na1110;
+
+public class JTInt extends JTCode {
+    private int value;
+
+    public JTInt(Integer integer) {
+        value = integer.intValue();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return Integer.toString(value);
+    }
+}
