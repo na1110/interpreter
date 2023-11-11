96b667e02d47897b0791467131b580b79844bd5f
na1110 <nagaitomoya1110@gmail.com>
2023-11-11 15:24:50 +0900

    Main.javaをファイルのインタープリタとして機能するように修正

--- src/interpreter/com/na1110/Main.java
+++ src/interpreter/com/na1110/Main.java
@@ -6,19 +6,27 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    static void usage() {
+        System.out.println("usage: java Main [source_filename]");
+    }
+
     public static void main(String[] args) {
         try {
-            BufferedReader in;
-            if (args.length == 0) {
-                in = new BufferedReader(new InputStreamReader(System.in));
+            if (args.length != 1) {
+                usage();
             } else {
-                in = new BufferedReader(new FileReader(args[0]));
+                BufferedReader in = new BufferedReader(new FileReader(args[0]));
+                Lexer lex = new Lexer(in);
+                Parser parser = new Parser();
+                while (true) {
+                    JTCode code = (JTCode) parser.parse(lex);
+                    if (code == null) {
+                        break;
+                    }
+                    System.out.println(code.run());
+                }
+                in.close();
             }
-            Lexer lex = new Lexer(in);
-
-
-
-            in.close();
         } catch (FileNotFoundException e) {
             System.out.println("can't open file '" + args[0] + "'");
         } catch (Exception e) {
