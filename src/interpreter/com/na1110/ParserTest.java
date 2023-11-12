4833fd143ed4e26f7456fdf64f26424f74d26e8f
na1110 <nagaitomoya1110@gmail.com>
2023-11-12 18:40:22 +0900

    ParserTest.javaを「Hello * 3」というテストに修正

--- src/interpreter/com/na1110/ParserTest.java
+++ src/interpreter/com/na1110/ParserTest.java
@@ -5,19 +5,19 @@ import java.io.StringReader;
 
 public class ParserTest {
     public void testParser() {
-        Reader reader = new StringReader("str = \"XYZ\"");
+        Reader reader = new StringReader("\"Hello\" * 3");
         Lexer lex = new Lexer(reader);
         Parser parser = new Parser();
         JTCode code = parser.parse(lex);
         if (code == null) {
-            System.out.println("fault");
+            System.out.println("fault: code is null");
         } else {
             try {
-                if (!code.run().toString().equals("XYZ")) {
-                    System.out.println("fault");
+                if (!code.run().toString().equals("HelloHelloHello")) {
+                    System.out.println("fault: " + code.run().toString());
                 }
             } catch (Exception e) {
-                System.out.println("fault");
+                System.out.println("fault: Exception occurred");
             }
         }
     }
