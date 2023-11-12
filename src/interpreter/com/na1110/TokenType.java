9fdf11980ad06a4c7252735254664dd7a9ddb3b3
na1110 <nagaitomoya1110@gmail.com>
2023-11-12 17:44:55 +0900

    TokenType.javaにSTRINGを追加

--- src/interpreter/com/na1110/TokenType.java
+++ src/interpreter/com/na1110/TokenType.java
@@ -4,4 +4,5 @@ class TokenType {
     public static final int EOS = -1;
     public static final int INT = 257;
     public static final int SYMBOL = 258;
+    public static final int STRING = 259;
 }
