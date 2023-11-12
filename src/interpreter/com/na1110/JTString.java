130115f7e855b3b1921b3e917c35bf8651261143
na1110 <nagaitomoya1110@gmail.com>
2023-11-12 18:00:59 +0900

    JTString.javaを初期化

---
+++ src/interpreter/com/na1110/JTString.java
@@ -0,0 +1,17 @@
+package com.na1110;
+
+public class JTString extends JTCode {
+    private String value;
+
+    public JTString(String string) {
+        value = string;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return value;
+    }
+}
