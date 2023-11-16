a508e4cabb637c2272cfa8cd0ec4daea4d695f96
na1110 <nagaitomoya1110@gmail.com>
2023-11-16 11:20:47 +0900

    TokenType.javaに論理演算記号を追加

--- src/interpreter/com/na1110/TokenType.java
+++ src/interpreter/com/na1110/TokenType.java
@@ -7,4 +7,10 @@ class TokenType {
     public static final int STRING = 259;
     public static final int TRUE = 260;
     public static final int FALSE = 261;
+    public static final int EQ = 262;
+    public static final int NE = 263;
+    public static final int LE = 264;
+    public static final int GE = 265;
+    public static final int AND = 266;
+    public static final int OR = 267;
 }
